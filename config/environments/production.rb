75f91ca3a2e6f1d3217ec838e8f440d574c3db52
Jayzz55 <vagrant@rails-dev-box>
2014-10-28 06:21:52 +0000

    update config environment for production mailer host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,8 @@ Superlist::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # http://jaya-bloccit.herokuapp.com/
+   config.action_mailer.default_url_options = { host: 'jaya-bloccit.herokuapp.com' }
+
 end
