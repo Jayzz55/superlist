2e32cfe28d6f9af13d0be118ad585069dac95e32
Jayzz55 <vagrant@rails-dev-box>
2014-10-28 12:31:47 +0000

    change db migrate from status to complete

--- db/migrate/20141024004004_create_todos.rb
+++ db/migrate/20141024004004_create_todos.rb
@@ -2,7 +2,7 @@ class CreateTodos < ActiveRecord::Migration
   def change
     create_table :todos do |t|
       t.text :body
-
+      t.boolean :complete, default: false
       t.timestamps
     end
   end
